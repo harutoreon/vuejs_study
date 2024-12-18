56ddd003c56eaa350a64cbad4d7766385fe4cca7
harutoreon <norabder567@gmail.com>
2024-12-19 05:18:35 +0900

    Modify HandlingJson.vue

--- src/HandlingJson.vue
+++ src/HandlingJson.vue
@@ -1,12 +1,27 @@
 <script setup>
-import users from './assets/users.json'
+import { ref, onMounted, watch } from 'vue'
+import axios from 'axios'
+
+const data = ref([])
+
+const loadData = async () => {
+  try {
+    const response = await axios.get('https://api.github.com/repos/ruby/ruby')
+    data.value = response.data
+  } catch (error) {
+    console.error('Failed to load data:', error)
+  }
+}
+
+onMounted(() => {
+  loadData()
+})
+
+watch(data, (newData) => {
+  console.log(newData['description'])
+})
 </script>
 
 <template>
-  <h1>User List</h1>
-  <ul>
-    <li v-for="user in users" v-bind:key="user.id">
-      {{ user.name }} - {{ user.email }}
-    </li>
-  </ul>
+  <p>{{ data.description }}</p>
 </template>
