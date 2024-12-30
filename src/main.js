5ba24bd6d27c71002ddbedc6e9196a85af482cc8
harutoreon <norabder567@gmail.com>
2024-12-31 06:50:12 +0900

    Add a template for a single-page application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
-import App from './AxiosInterceptors.vue'
+import router from './router'
+import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
