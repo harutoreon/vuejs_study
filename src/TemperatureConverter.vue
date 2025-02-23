eef0079779f1fcf75fbf410c8b029c483b5742da
reonharuto <norabder567@gmail.com>
2025-02-23 15:00:08 +0900

    Add TemperatureConverter.vue

---
+++ src/TemperatureConverter.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const c = ref(0)
+const f = ref(32)
+
+function setC(event, v = +event.target.value) {
+  c.value = v
+  f.value = v * (9 / 5) + 32
+}
+
+function setF(event, v = +event.target.value) {
+  f.value = v
+  c.value = (v - 32) * (5 / 9)
+}
+</script>
+
+<template>
+  <input type="number" v-bind:value="c" v-on:change="setC"> Celsius =
+  <input type="number" v-bind:value="f" v-on:change="setF"> Fahrenheit
+</template>
